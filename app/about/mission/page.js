cda72f508f07ea0599ff805c9992f5956389939a
Talat09 <talat181008312@gmail.com>
2024-04-07 02:29:01 +0600

    implement next js image optimization

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,18 +1,26 @@
 import Button from "@/app/components/Button";
 //@(root folder) is default import alias
+import Image from "next/image";
+import thumb from "@/public/images/Talat-Mahmud.jpg";
 const page = () => {
   return (
-    <main className="mt-10">
-      <div className="font-semibold"> Mission Page</div>
+    <main className="mt-10 ">
+      <div className="text-4xl font-semibold"> Mission Page</div>
       <hr />
+
       <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
-        nostrum porro error, laborum ad minima fuga aliquam nisi hic cumque,
-        corporis iure quis tempora? Reprehenderit delectus repellat officiis
-        facere totam nam, veritatis aliquam earum iure debitis. Dolor pariatur,
-        optio amet laboriosam rerum corporis fugit ex natus. Assumenda sapiente
-        maiores quisquam!
+        <p className="my-2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+          nostrum porro error, laborum ad minima fuga aliquam nisi hic cumque,
+          corporis iure quis tempora? Reprehenderit delectus repellat officiis
+          facere totam nam, veritatis aliquam earum iure debitis. Dolor
+          pariatur, optio amet laboriosam rerum corporis fugit ex natus.
+          Assumenda sapiente maiores quisquam!
+        </p>
+
+        <Image placeholder="blur" src={thumb} alt="Thumb image" />
       </div>
+
       <Button></Button>
     </main>
   );
