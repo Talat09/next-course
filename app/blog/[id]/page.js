02883b1d6dc95971ff0aba722f64e5aac0385fe5
Talat09 <talat181008312@gmail.com>
2024-04-06 23:49:33 +0600

    add dynamic blog not found page setup

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,7 +1,9 @@
-import React from "react";
-
+import { notFound } from "next/navigation";
 const page = ({ params }) => {
   const { id } = params;
+  if (id === "4") {
+    notFound();
+  }
   return <div>This is description of blog details page id: {id}.</div>;
 };
 
